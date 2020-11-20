006630b0f6ec0fe12cf26db33d23210252d0b9f8
dmouriss <dieter.mourisse@howest.be>
2020-11-20 11:57:05 +0100

    Add project files.

---
+++ Pra.DBConnected.Wpf/WinCategories.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pra.DBConnected.Wpf
+{
+    /// <summary>
+    /// Interaction logic for WinCategory.xaml
+    /// </summary>
+    public partial class WinCategory : Window
+    {
+        public WinCategory()
+        {
+            InitializeComponent();
+        }
+    }
+}
